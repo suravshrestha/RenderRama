43ba2724b8cba300ef59a05cedfc6dced92a4444
Surav Shrestha <surav_02@live.com>
2023-08-07 13:06:01 +0545

    feat: points input table

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import PointsInputTable from "./components/PointsInputTable";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="m-10 mx-20 tracking-wide">
+      <h1 className="text-3xl font-bold underline text-center">RenderRama</h1>
+      <div className="grid grid-cols-8 gap-4 mt-5">
+        <div className="w-full">
+          <PointsInputTable />
+        </div>
+        <div className="bg-green-100 col-span-5">Canvas</div>
+        <div className="bg-sky-100 col-span-2">Translate</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/PointsInputTable.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+
+interface Point {
+  x: number | string;
+  y: number | string;
+}
+
+const PointsInputTable: React.FC = () => {
+  const [points, setPoints] = useState<Point[]>([
+    // Initial empty rows
+    { x: "", y: "" },
+    { x: "", y: "" },
+  ]);
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+    key: keyof Point // x or y
+  ) => {
+    // Handle empty input
+    if (event.target.value === "") {
+      const newPoints = points.slice();
+      newPoints[index][key] = "";
+      return setPoints(newPoints);
+    }
+
+    // Handle point range
+    const value = parseInt(event.target.value);
+    if (value > 500 || value < 0) {
+      return;
+    }
+
+    // Update state
+    const newPoints = points.slice();
+    newPoints[index][key] = value;
+    setPoints(newPoints);
+  };
+
+  // Append row if table is full
+  if (points[points.length - 1].y) {
+    points.push({ x: "", y: "" });
+  }
+
+  return (
+    <table className="border-collapse border border-slate-500 p-1">
+      <thead className="h-8">
+        <tr>
+          <th className="border border-slate-700" colSpan={2}>
+            <div className="flex items-center justify-center space-x-10">
+              <span>Points</span>
+              <button
+                type="button"
+                className="tracking-wider text-red-600 underline"
+                onClick={() =>
+                  setPoints([
+                    { x: "", y: "" },
+                    { x: "", y: "" },
+                  ])
+                }
+              >
+                Reset
+              </button>
+            </div>
+          </th>
+        </tr>
+      </thead>
+      <thead className="h-8">
+        <tr>
+          <th className="border border-slate-700">X</th>
+          <th className="border border-slate-700">Y</th>
+        </tr>
+      </thead>
+      <tbody>
+        {points.map((coord, index) => (
+          <tr key={index}>
+            <td className="border border-slate-700">
+              <input
+                className="w-full text-center"
+                value={coord.x}
+                onChange={(e) => {
+                  handleInputChange(e, index, "x");
+                }}
+              />
+            </td>
+            <td className="border border-slate-700">
+              <input
+                className="w-full text-center"
+                value={coord.y}
+                onChange={(e) => handleInputChange(e, index, "y")}
+              />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default PointsInputTable;
